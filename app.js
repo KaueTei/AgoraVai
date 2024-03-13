1b74e42973a4d164d21076f8031a1a3e016e22c6
txKaue <kauetxp@gmail.com>
2024-03-13 14:19:24 -0300

    Teste vai

---
+++ app.js
@@ -0,0 +1,47 @@
+// Importando os módulos necessários
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Inicializando o aplicativo Express
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware para analisar o corpo das solicitações
+app.use(bodyParser.json());
+
+// Conectando ao banco de dados MongoDB
+mongoose.connect('mongodb://<username>:<password>@<hostname>:<port>/<database>', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro de conexão com o MongoDB:'));
+db.once('open', function() {
+  console.log('Conectado ao MongoDB');
+});
+
+// Definindo o esquema do modelo
+const CarCrashSchema = new mongoose.Schema({
+  date: { type: Date, default: Date.now }
+});
+const CarCrash = mongoose.model('CarCrash', CarCrashSchema);
+
+// Rota para lidar com a solicitação POST do ESP8266
+app.post('/crash', async (req, res) => {
+  try {
+    // Criar uma nova instância do modelo CarCrash
+    const newCrash = new CarCrash();
+    // Salvar no banco de dados
+    await newCrash.save();
+    console.log('Dados do acidente salvos no banco de dados.');
+    // Enviar resposta 200 (OK) de volta para o ESP8266
+    res.sendStatus(200);
+  } catch (error) {
+    console.error('Erro ao salvar os dados do acidente no banco de dados:', error);
+    // Enviar resposta 500 (Erro interno do servidor) de volta para o ESP8266 em caso de erro
+    res.sendStatus(500);
+  }
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
